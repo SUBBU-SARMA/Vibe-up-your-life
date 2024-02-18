f6da4eb8087a6b6f25b6ddafc82f9206e7401c7a
unknown <senagavaramsarma94@gmail.com>
2024-02-18 23:59:00 +0530

    DetailScreen changes

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,19 +1,30 @@
 import { View, FlatList, StyleSheet } from 'react-native';
 
-import { MEALS } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
+import { useEffect, useLayoutEffect } from 'react';
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
   function renderMealItem(itemData) {
     const item = itemData.item;
 
     const mealItemProps = {
+      id: item.id,
       title: item.title,
       imageUrl: item.imageUrl,
       affordability: item.affordability,
